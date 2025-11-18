64abb6df6fd8b742cd1de976de85c88591c92b12
maker-shihab <frontendmaker99@gmail.com>
2025-11-18 12:25:40 +0600

    update for error checking

---
+++ tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs"],
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  outDir: "dist",
+  target: "es2022",
+  minify: false,
+  bundle: true,
+  treeshake: true,
+});
